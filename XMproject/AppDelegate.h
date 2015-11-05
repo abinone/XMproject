3bd7e1a2f8c770c82f8d6200b7d0c5a843fabe28
abin <your_email@youremail.com>
2015-11-05 15:58:03 +0800

    Initial Commit

---
+++ XMproject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMproject
+//
+//  Created by a bin on 15/11/5.
+//  Copyright (c) 2015年 a bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMproject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMproject
+//
+//  Created by a bin on 15/11/5.
+//  Copyright (c) 2015年 a bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
